27dd6383a730c868cee0b05b93145b264ef30ef4
Neelima <nprasad@ucsd.edu>
2021-02-13 23:21:42 -0700

    adding files

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,26 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor() {
+    const template = document.createElement('template');
+    template.innerHTML = `
+    <!-- li class="product">
+                   <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
+                   <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
+                   <p class="price">$109.95</p>
+                   <button onclick="alert('Added to Cart!')">Add to Cart</button>
+               </li -->`;
+   super();
+   
+   this.root = this.attachShadow({ mode: 'open' });
+   this.root.appendChild(template.content.cloneNode(true));
+  
+               
+    this.root.appendChild(template.content.cloneNode(true));
+    
+  }
+
+ 
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,13 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => {
+    window.localStorage.setItem("array", JSON.stringify(data));
+    
+    
+  })
+    
+  
 });
