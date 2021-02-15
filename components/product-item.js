82fc9d54776b4d81df28815146ee3985f3055a80
Neelima <nprasad@ucsd.edu>
2021-02-14 21:29:28 -0700

    final commit

--- components/product-item.js
+++ components/product-item.js
@@ -1,27 +1,168 @@
 // product-item.js
-
-class ProductItem extends HTMLElement {
-  // TODO
-  constructor() {
-    const template = document.createElement('template');
-    template.innerHTML = `
-    <!-- li class="product">
-                   <img src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops" width=200>
-                   <p class="title">Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops</p>
-                   <p class="price">$109.95</p>
-                   <button onclick="alert('Added to Cart!')">Add to Cart</button>
-               </li -->`;
-   super();
-   
-   this.root = this.attachShadow({ mode: 'open' });
-   this.root.appendChild(template.content.cloneNode(true));
-  
-               
-    this.root.appendChild(template.content.cloneNode(true));
-    
+const template = document.createElement("template");
+template.innerHTML = `
+<li class="product">
+  <img src="" alt="">
+  <p class="title"></p>
+  <p class="price"></p>
+</li>
+<style>
+  .price {
+    color: green;
+    font-size: 1.8em;
+    font-weight: bold;
+    margin: 0;
   }
-
- 
+  .product {
+    align-items: center;
+    background-color: white;
+    border-radius: 5px;
+    display: grid;
+    grid-template-areas: 
+    'image'
+    'title'
+    'price'
+    'add';
+    grid-template-rows: 67% 11% 11% 11%;
+    height: 450px;
+    filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+    margin: 0 30px 30px 0;
+    padding: 10px 20px;
+    width: 200px;
+  }
+  .product > button {
+    background-color: rgb(255, 208, 0);
+    border: none;
+    border-radius: 5px;
+    color: black;
+    justify-self: center;
+    max-height: 35px;
+    padding: 8px 20px;
+    transition: 0.1s ease all;
+  }
+  .product > button:hover {
+    background-color: rgb(255, 166, 0);
+    cursor: pointer;
+    transition: 0.1s ease all;
+  }
+  .product > img {
+    align-self: center;
+    justify-self: center;
+    width: 100%;
+  }
+  .title {
+    font-size: 1.1em;
+    margin: 0;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .title:hover {
+    font-size: 1.1em;
+    margin: 0;
+    white-space: wrap;
+    overflow: auto;
+    text-overflow: unset;
+  }
+  .price {
+  color: green;
+  font-size: 1.8em;
+  font-weight: bold;
+  margin: 0;
 }
 
-customElements.define('product-item', ProductItem);
+.product {
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  display: grid;
+  grid-template-areas: 
+  'image'
+  'title'
+  'price'
+  'add';
+  grid-template-rows: 67% 11% 11% 11%;
+  height: 450px;
+  filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+  margin: 0 30px 30px 0;
+  padding: 10px 20px;
+  width: 200px;
+}
+
+.product > button {
+  background-color: rgb(255, 208, 0);
+  border: none;
+  border-radius: 5px;
+  color: black;
+  justify-self: center;
+  max-height: 35px;
+  padding: 8px 20px;
+  transition: 0.1s ease all;
+}
+
+.product > button:hover {
+  background-color: rgb(255, 166, 0);
+  cursor: pointer;
+  transition: 0.1s ease all;
+}
+
+.product > img {
+  align-self: center;
+  justify-self: center;
+  width: 100%;
+}
+
+.title {
+  font-size: 1.1em;
+  margin: 0;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.title:hover {
+  font-size: 1.1em;
+  margin: 0;
+  white-space: wrap;
+  overflow: auto;
+  text-overflow: unset;
+}
+</style>
+
+`;
+class ProductItem extends HTMLElement {
+  
+  static get observedAttributes() {
+    return ["image-src", "title", "price", "incart"];
+  }
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+
+  attributeChangedCallback(att_name, old_val, new_val) {
+    switch (att_name) {
+      case "image-src":
+        shadow.querySelector("li>img").src = new_val;
+        break;
+      case "title":
+        shadow.querySelector("li>img").alt = new_val;
+        break;
+      case "price":
+        shadow.querySelector("p.price").innerText = new_val;
+        break;
+      
+      case "incart":
+          break;
+        
+      default:
+    
+    }
+  }
+
+  
+}
+
+customElements.define("product-item", ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,27 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let array = JSON.parse(localStorage.getItem('products'));
+  if (window.localStorage.getItem("array") == null) {
   fetch('https://fakestoreapi.com/products')
   .then(response => response.json())
-  .then(data => {
+  .then((data) => {
     window.localStorage.setItem("array", JSON.stringify(data));
+    array = data
     
-    
-  })
+  });
+  let productListHolder = document.getElementById("product-list");
+  const products = products.map((product) => {
+    const item = document.createElement('product-item'); 
+    item.setAttribute('img-src', product.image);
+    item.setAttribute('title', product.title);
+    item.setAttribute('price', product.price);
+    item.setAttribute('incart', array.includes(product.id));
+    productListHolder.appendChild(item);
+
+    return item;
+  });
+}
     
   
 });
